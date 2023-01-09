82a2a82730c8968d98681d229856e3547b58968b
Ted <withwooyong@yanadoocorp.com>
2023-01-09 21:53:52 +0900

    20230109

--- src/main/java/com/example/demo/entity/test/Temp4.java
+++ src/main/java/com/example/demo/entity/test/Temp4.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -21,6 +22,7 @@ public class Temp4 {
     @GeneratedValue(generator = "uuid2")
 //    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "id", updatable = false, nullable = false, columnDefinition = "VARCHAR(36)")
     @Type(type = "uuid-char")
     private UUID id;
     private String name;
